01b8901f812db4cd1b52f70c1eecc0b2523393d4
Tomás Vásquez <tomas.vasquez2101@alumnos.ubiobio.cl>
2025-10-18 00:54:52 -0300

    Dto

---
+++ Microservicios/microservice-inventario/src/main/java/com/microservice/dto/CompraMateriaPrimaResponseDTO.java
@@ -0,0 +1,23 @@
+package com.microservice.dto;
+
+import com.microservice.entity.CompraMateriaPrima;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompraMateriaPrimaResponseDTO {
+    private Long id;
+    private String numDoc;
+    private CompraMateriaPrima.TipoDocumento tipoDoc;
+    private String proveedor;
+    private LocalDate fechaCompra;
+    private LocalDate fechaPago;
+    private LocalDate createdAt;
+    private Integer totalLotes;
+    private Double montoTotal;
+    private List<LoteMateriaPrimaDTO> lotes;
+}

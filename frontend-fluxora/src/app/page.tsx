38dfaa683c5bd4b90ca028a0430c130d193559ee
Tomás Vásquez <tomas.vasquez2101@alumnos.ubiobio.cl>
2025-11-17 15:06:45 -0300

    prueba

--- frontend-fluxora/src/app/page.tsx
+++ frontend-fluxora/src/app/page.tsx
@@ -18,8 +18,12 @@ export default function LoginPage() {
     setError(null);
     try {
       console.log("API URL:", API_BASE_URL);
+      console.log(
+        ">>> API URL EXACTA:",
+        JSON.stringify(process.env.NEXT_PUBLIC_API_URL)
+      );
       const res = await fetch(
-        `${API_BASE_URL}/api/usuarios/auth/login`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/usuarios/auth/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
